2036ff58f6af4a78e10ef49b22294685696f9b93
Adrien Bufort <adrien.bufort@orange.com>
2021-12-07 12:08:41 +0000

    change setup.py to add requierements file

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup(name='fourier_neural_operator',
-      version='0.1',
-      description='Library and exemples to use the fourier neural operator',
-      author='zongyi-li',
-      author_email='',
-      url='https://zongyi-li.github.io',
-      packages=['fourier_neural_operator'],
-      package_dir={'fourier_neural_operator': 'fourier_neural_operator'},
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name = "fourier_neural_operator",
+    version = "0.1",
+    description = ("Library and exemples to use the fourier neural operator"),
+    packages=['fourier_neural_operator'],
+    url='https://zongyi-li.github.io',
+    long_description=read('README.md'),
+    package_dir={'fourier_neural_operator': 'fourier_neural_operator'},
+    install_requires=required,
 )
