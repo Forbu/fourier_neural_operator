bd3c6384a0a82d7588817063a0c3201a15a23100
Adrien B <forbu14@gmail.com>
2021-12-08 17:05:25 +0100

    correction setup to read the requirements file

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+here = os.path.abspath(os.path.dirname(__file__))
+
+# What packages are required for this module to be executed?
+try:
+    with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+        required = f.read().split('\n')
+except:
+    required = []
 
 setup(
     name = "fourier_neural_operator",
