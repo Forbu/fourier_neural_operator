8ff1d22504b08315df7f1f5c96156e58c649570a
Adrien Bufort <adrien.bufort@orange.com>
2021-12-08 16:30:50 +0000

    remove old main

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except:
 
 setup(
     name = "fourier_neural_operator",
-    version = "0.2",
+    version = "0.4",
     description = ("Library and exemples to use the fourier neural operator"),
     packages=['fourier_neural_operator'],
     url='https://zongyi-li.github.io',
