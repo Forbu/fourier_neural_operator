d24c038566ed658004ea27a7158d14cdfde5f101
Adrien Bufort <adrien.bufort@orange.com>
2022-01-26 16:55:44 +0000

    correction layer

--- fourier_neural_operator/layers/fourier_2d.py
+++ fourier_neural_operator/layers/fourier_2d.py
@@ -20,7 +20,7 @@ from timeit import default_timer
 # fourier layer
 ################################################################
 class SpectralConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, modes):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
         super(SpectralConv2d, self).__init__()
 
         """
@@ -29,11 +29,12 @@ class SpectralConv2d(nn.Module):
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.modes = modes #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
 
         self.scale = (1 / (in_channels * out_channels))
-        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes, self.modes, dtype=torch.cfloat))
-        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes, self.modes, dtype=torch.cfloat))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
 
     # Complex multiplication
     def compl_mul2d(self, input, weights):
@@ -47,11 +48,12 @@ class SpectralConv2d(nn.Module):
 
         # Multiply relevant Fourier modes
         out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
-        out_ft[:, :, :self.modes, :self.modes] = \
-            self.compl_mul2d(x_ft[:, :, :self.modes, :self.modes], self.weights1)
-        out_ft[:, :, -self.modes:, :self.modes] = \
-            self.compl_mul2d(x_ft[:, :, -self.modes:, :self.modes], self.weights2)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
 
         #Return to physical space
         x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
         return x
+
